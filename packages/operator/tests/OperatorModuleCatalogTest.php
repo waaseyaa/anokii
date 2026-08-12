cb429daf8d3b38ae7a2a9cc8f593dca30346e802
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2026-08-12 19:05:53 -0400

    feat(operator): ship brandable default workspace shell

--- packages/operator/tests/OperatorModuleCatalogTest.php
+++ packages/operator/tests/OperatorModuleCatalogTest.php
@@ -8,9 +8,13 @@ use Anokii\Operator\Http\OperatorResponsePolicy;
 use Anokii\Operator\Module\OperatorModule;
 use Anokii\Operator\Module\OperatorModuleCatalog;
 use Anokii\Operator\Module\OperatorModuleProviderInterface;
+use Anokii\Operator\Shell\OperatorShell;
+use Anokii\Operator\Template\OperatorTemplates;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 use Waaseyaa\Access\AuthorizationPrincipal;
 use Waaseyaa\Access\AuthorizationPrincipalInterface;
 
@@ -18,6 +22,8 @@ require_once __DIR__.'/../src/Module/OperatorModule.php';
 require_once __DIR__.'/../src/Module/OperatorModuleProviderInterface.php';
 require_once __DIR__.'/../src/Module/OperatorModuleCatalog.php';
 require_once __DIR__.'/../src/Http/OperatorResponsePolicy.php';
+require_once __DIR__.'/../src/Shell/OperatorShell.php';
+require_once __DIR__.'/../src/Template/OperatorTemplates.php';
 
 final class OperatorModuleCatalogTest extends TestCase
 {
@@ -74,5 +80,33 @@ final class OperatorModuleCatalogTest extends TestCase
         self::assertStringContainsString('no-store', (string) $response->headers->get('Cache-Control'));
         self::assertSame('noindex, nofollow', $response->headers->get('X-Robots-Tag'));
         self::assertSame('same-origin', $response->headers->get('Referrer-Policy'));
+    }
+
+    #[Test]
+    public function hostBrandingOverridesDefaultsWithoutReplacingAuthorizedModules(): void
+    {
+        $modules = [new OperatorModule('website', 'Website', 'Daily work', '/admin/anokii/website', 'Manage the public website.')];
+        $context = OperatorShell::context($modules, '', 'Matthew Owl', 'Communications Officer', [
+            'brand_title' => 'Sheguiandah First Nation',
+            'brand_tag' => 'Anokii workspace',
+            'brand_logo_src' => '/media/sfn-logo.png',
+            'brand_logo_alt' => 'Sheguiandah First Nation',
+            'theme_href' => '/assets/sfn-operator.css',
+            'nav' => [],
+            'tiles' => [],
+        ]);
+
+        self::assertSame('Sheguiandah First Nation', $context['brand_title']);
+        self::assertSame(['website'], array_column($context['nav'], 'id'));
+        self::assertSame(['website'], array_column($context['tiles'], 'id'));
+
+        $twig = new Environment(new FilesystemLoader());
+        OperatorTemplates::register($twig);
+        $html = $twig->render('@anokii_operator/dashboard.html.twig', $context);
+        self::assertStringContainsString('Sheguiandah First Nation', $html);
+        self::assertStringContainsString('/media/sfn-logo.png', $html);
+        self::assertStringContainsString('/assets/sfn-operator.css', $html);
+        self::assertStringContainsString('/admin/anokii/website', $html);
+        self::assertStringContainsString('Open workspace navigation', $html);
     }
 }
